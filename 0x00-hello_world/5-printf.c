f0f17cdec1d0a8aad86b4d0da593ff2588799324
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-05 20:13:28 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ *main - uses printf
+ *Description: same exercise doing printf
+ *Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
