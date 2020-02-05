59e69f3f4026b7fc171c65eab32261a5d4c37e9a
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-05 20:56:28 +0000

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - sizeof
+ *Description: using sizeof
+ *Return: void
+ */
+int main(void)
+int i;
+float fl;
+char ch;
+long int lon;
+long long int lli;
+printf("Size of char: %ld bytes\n", sizeof(ch));
+printf("Size of int: %ld bytes\n", sizeof(i));
+printf("Size of long int: %ld bytes\n", sizeof(lon));
+printf("Size of long long int: %ld byte\n", sizeof(lli));
+printf("Size of long float: %ld byte\n", sizeof(fl));
+return (0);
