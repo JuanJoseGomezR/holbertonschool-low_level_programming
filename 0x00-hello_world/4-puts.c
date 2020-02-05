84355b057ba073e76d36e0d2c92fbce285d235d2
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-05 19:16:46 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
-#!/bin/bash
-int main(){
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
