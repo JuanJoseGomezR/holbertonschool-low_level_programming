f0d0d4cc8931d153fb4c9e66d1172bd2ddc9a459
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-06 23:50:43 +0000

    commit1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - Outputs
+* Return: 0
+*/
+int main(void)
+{
+int a, b, c;
+a = 0;
+while (a < 100)
+{
+b = a % 10;
+c = a / 10;
+if (c != b && c < b)
+{
+putchar(c + '0');
+putchar(b + '0');
+if (a < 89)
+{
+putchar(',');
+putchar(' ');
+}
+}
+a++;
+}
+putchar('\n');
+return (0);
+}
+
