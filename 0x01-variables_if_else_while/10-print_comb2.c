8a20b69850dabe9841e9e71fa8aa1c5031f8a082
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-06 22:49:54 +0000

    commit1

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,15 +8,17 @@ int main(void)
 int i;
 int j;
 
-for (i = 48; i < 59 ; i++)
+for (i = 48; i < 58; i++)
 {
-for (j = 48; j < 59; j++)
+for (j = 48; j < 58; j++)
 {
 putchar(i);
 putchar(j);
+if (i < 57 || j < 57)
 putchar(',');
 putchar(' ');
 }
 }
+putchar('\n');
 return (0);
 }
