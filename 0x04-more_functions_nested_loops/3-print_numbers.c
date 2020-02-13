92658707902efdd43dc16b8ddb6fa7a4dbd7195a
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-13 14:38:29 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* print_numbers - print 0 - 9
+* Return: nothing
+*/
+
+void print_numbers(void)
+{
+int c;
+
+for (c = 48; c <= 58; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
