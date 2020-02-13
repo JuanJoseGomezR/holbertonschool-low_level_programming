c1766bf20118f6b3276c8e75182cebfc988a50f3
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-13 14:50:06 +0000

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,12 +4,11 @@
 * print_numbers - print 0 - 9
 * Return: nothing
 */
+char c;
 
 void print_numbers(void)
 {
-int c;
-
-for (c = 48; c <= 58; c++)
+for (c = '0'; c <= '9'; c++)
 {
 _putchar(c);
 }
