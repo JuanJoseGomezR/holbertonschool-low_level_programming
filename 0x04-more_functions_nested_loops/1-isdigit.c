9253aecfb8467b96e010b1d0c5f0b4bee3c364c7
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-13 14:00:19 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* _isdigit - digit or not
+* @c:varible
+* Return: 0 - 1
+*/
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 58)
+{
+return (1);
+}
+else
+return (0);
+}
