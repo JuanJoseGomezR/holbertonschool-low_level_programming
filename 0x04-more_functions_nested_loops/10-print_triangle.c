4e7a758a53a7b0600569542ce7141c655d23d6f2
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-13 22:13:18 +0000

    commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,35 +1,35 @@
 #include "holberton.h"
 
 /**
- * print_square - prints "#"
- * @size: size of the #
- * Return: nothing
- */
+* print_square - prints "#"
+* @size: size of the #
+* Return: nothing
+*/
 
 void print_triangle(int size)
 {
-  int row, column;
+int row, column;
 
-  if (size < 0)
-    {
-      _putchar('\n');
-    }
-  else
-    {
-  for (row = 1 ; row <= size; row++)
-    {
-      for (column = 1; column <= size; column++)
-	{
-	  if (column != size && column <= size - row)
-	    {
-	      _putchar(' ');
-	    }
-	  else
-	    {
-	      _putchar('#');
-	    }
-	}
-      _putchar('\n');
-    } 
-    }
+if (size < 0)
+{
+_putchar('\n');
+}
+else
+{
+for (row = 1 ; row <= size; row++)
+{
+for (column = 1; column <= size; column++)
+{
+if (column != size && column <= size - row)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
 }
