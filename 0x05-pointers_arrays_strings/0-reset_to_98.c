290204ea47eb1331a161a5413561e1abc132b2f8
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-17 20:30:25 +0000

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* reset_to_98 - Change to 98 the value of the variable
+* @*n: Variable
+* return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	if(n)
+	{
+		*n = 98;
+	}
+}
+	
