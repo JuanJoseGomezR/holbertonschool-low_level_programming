bffc5a23442223db74b9c2cc1307186b5e9599ff
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-17 20:48:32 +0000

    commit1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * swap_int - swaps the values
+ * @a: var 1
+ * @b: var 2
+ * return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	if (a)
+	{
+		*a = 98;
+		*a = 42;
+	}
+	if (b)
+	{
+		*b = 42;
+		*b = 98;
+	}
+}
