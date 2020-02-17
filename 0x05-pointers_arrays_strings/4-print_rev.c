f47034162c980cf06caa73c26380adae2bc64f83
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-17 22:32:18 +0000

    commit1

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+  * print_rev - print in reverse
+  * @s: variable
+  * return: nothing
+  */
+
+void print_rev(char *s)
+{
+char *t = s;
+	if (s)
+	{
+		while (*t)
+		{
+		t++;
+		}
+		while (s < t--)
+		{
+			_putchar(*t);
+		}
+	}
+}
