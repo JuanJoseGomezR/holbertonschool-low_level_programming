88431d9819db35890f64b0646a0436926725e667
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-18 20:22:16 +0000

    commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,13 @@ void puts2(char *str)
 {
 	if (str)
 	{
-		do
-		{
-		if (*str)
-		{
-			_putchar(*str++);
+		do {
+			if (*str)
+			{
+				_putchar(*str++);
+			}
 		}
-		}
-			while (*str++);
+		while (*str++);
 			_putchar('\n');
-		}
+	}
 }
