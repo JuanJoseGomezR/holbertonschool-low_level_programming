7cbaa710c9ae1bfb6fac4f62914c0ae73cc28f7c
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-20 23:18:37 +0000

    commit

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+  * rot13 - encodes a string
+  * @s: point
+  * Return: s
+  */
+
+char *rot13(char *s)
+{
+int i, j;
+char array[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char arr[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (array[j] == s[i])
+			{
+				s[i] = arr[j];
+			}
+		}
+	}
+	return (s);
+}
