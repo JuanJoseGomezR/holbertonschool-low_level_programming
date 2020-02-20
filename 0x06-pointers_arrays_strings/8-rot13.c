e28c1c0275d451f23f2d1404f887291023e02e1e
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-20 23:22:43 +0000

    commit2

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -11,7 +11,7 @@ int i, j;
 char array[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char arr[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != 0; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
