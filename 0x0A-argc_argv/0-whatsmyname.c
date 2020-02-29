a5edfc42abf2a97f006b36f625daccc5accea18a
JuanJoseGomezR <juangomez0149@gmail.com>
2020-02-29 23:55:43 +0000

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - shows file's name
+  * @argc: argument length
+  * @argv: contains arguments
+  * Return: 0
+  */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
