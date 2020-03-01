d8a0123809f5e6dcc0c89e229b79fc23274bbadd
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-01 00:11:42 +0000

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - number of arg
+  * @argc: # of arguments
+  * @argv contains arguments
+  * Return: 0
+  */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+int count;
+	for (count = 0; count < argc; count++)
+	{
+		if (count + 1 == argc)
+			printf("%d\n", count);
+	}
+	return (0);
+}
