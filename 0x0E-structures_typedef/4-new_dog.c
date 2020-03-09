3ca2aace9f9c36822484cc55301ac4508f0ff23a
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-09 23:11:44 +0000

    commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -55,8 +55,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(sizeof(char) * _strlen(owner) + 1);
 	if (dog->owner == NULL)
 	{
-		free(dog->name);
 		free(dog);
+		free(dog->name);
 		return (NULL);
 	}
 	_strcpy(dog->name, name);
