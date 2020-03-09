417b004530e43ab5ede351b6f58c667b05065dae
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-09 21:32:59 +0000

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,14 @@
   * @age: the age of the dog
   * @owner: owner of the dog
   */
-typedef struct dog
+typedef struct dog dog_t
 {
 char *name;
 float age;
 char *owner;
-} dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
