37ac130a25d6c5644313c37f4e2706e7d57a16a7
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-11 15:58:56 +0000

    commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,10 @@
 #include "function_pointers.h"
 /**
-  *
-  *
-  *
-  *
+  * array_iterator - executes a function given as a parameter
+  * @array: array
+  * @size: size of array
+  * @action: funct point
+  * Return: nothing
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
