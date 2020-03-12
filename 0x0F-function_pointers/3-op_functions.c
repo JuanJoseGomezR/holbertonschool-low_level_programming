88bc03912f8643642668bd61fee75478f7785d90
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-12 01:23:25 +0000

    commit

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -39,10 +39,10 @@ int op_mul(int a, int b)
   */
 int op_div(int a, int b)
 {
-	if (b == 0)
+	if (!b)
 	{
-		printf("Error\n");
-		exit (100);
+		puts("Error");
+		exit(100);
 	}
 	return (a / b);
 }
@@ -54,10 +54,10 @@ int op_div(int a, int b)
   */
 int op_mod(int a, int b)
 {
-	if (b == 0)
+	if (!b)
 	{
-		printf("Error\n");
-		exit (100);
+		puts("Error");
+		exit(100);
 	}
 	return (a % b);
 }
