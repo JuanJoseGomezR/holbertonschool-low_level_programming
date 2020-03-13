e164b7d5c1a09d0aeae0c5ab9ab29a926bf99037
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-13 14:48:00 +0000

    commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+  * print_numbers - print num with spaces
+  * @separator: spaces
+  * @n: # of elementws
+  * Return: void
+  */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list num;
+
+	if (separator == NULL)
+		separator = "";
+
+	va_start(num, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(num, int));
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(num);
+}
