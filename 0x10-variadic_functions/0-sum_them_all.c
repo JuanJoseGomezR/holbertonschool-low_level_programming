5a6189d580eaed0f9f43f46616a3dce47c7f21c4
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-13 14:28:11 +0000

    commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+  * sum_them_all - sum all the arg
+  * @n: consta
+  * Return: sum
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list valist;
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(valist, int);
+	}
+	va_end(valist);
+	return (sum);
+}
