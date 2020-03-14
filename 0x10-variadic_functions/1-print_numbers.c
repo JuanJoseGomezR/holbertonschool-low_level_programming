a7fc685d852aaaa1886381c31dcd2666decbd9d9
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-14 20:12:58 +0000

    commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(num, int));
+		if (i < n - 1)
+		{
 		printf("%s", separator);
+		}
 	}
 	printf("\n");
 	va_end(num);
