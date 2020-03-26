7c379e28d105667720a7a2ae87338cd546f2b0ea
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-26 21:15:19 +0000

    Upload Binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+  *
+  *
+  *
+  */
+void print_binary(unsigned long int n)
+{
+	int count = 0;
+	unsigned long int store;
+
+	store = n;
+	while (store > 0)
+	{
+		store >>= 1;
+		count++;
+	}
+	if (count != 0)
+		count--;
+	while (count >= 0)
+	{
+		if ((n >> count) & 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		count--;
+	}
+}
