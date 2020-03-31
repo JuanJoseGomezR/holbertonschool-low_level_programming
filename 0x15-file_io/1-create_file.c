ceff791d58cad1247645587a12a579c436e1e670
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-31 21:50:31 +0000

    upload fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,29 @@
 #include "holberton.h"
 /**
-* create_file - creates a file
-* @filename: points
-* @text_content: content
-* Return: integer
-*/
+ * create_file - creates a file
+ * @filename: points
+ * @text_content: content
+ * Return: integer
+ */
 
 int create_file(const char *filename, char *text_content)
 {
-    int text_length, fd;
+	int text_length, fd;
 
-    if (filename == NULL)
-        return (-1);
-    if (text_content == NULL)
-        text_content = "";
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 
-    while (*text_content != '\0')
-        text_length++;
+	while (*text_content != '\0')
+		text_length++;
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    if (fd == -1)
-        return (-1);
+	if (fd == -1)
+		return (-1);
 
-    write(fd, text_content, text_length);
-    close(fd);
-    return (1);
+	write(fd, text_content, text_length);
+	close(fd);
+	return (1);
 }
