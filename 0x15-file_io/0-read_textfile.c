28811d14673d8993eb8143871d70c215dd256caa
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-31 20:19:12 +0000

    Uploading first try

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,2 +1,44 @@
-#include <stdio.h>
-#include
+#include "holberton.h"
+/**
+* read_textfile - reads t4extfile
+* @filename: name of the file
+* @letter: letters
+* Return: |
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    char *buffer = NULL;
+    ssize_t b_read;
+    ssize_t b_written;
+    int var;
+
+    if (!(filename && letters))
+        return (0);
+    
+    var = open(filename, O_RDONLY);
+    if (var == -1)
+        return (0);
+    
+    buffer = malloc(sizeof(char) * letters);
+    if (!buffer)
+        return (0);
+    
+    b_read = read(var, buffer, letters);
+    close(var);
+
+    if (b_read < 0)
+    {
+        free(buffer);
+        return (0);
+    }
+    if (!b_read)
+        b_read = letters;
+    
+    b_written = write(STDOUT_FILENO, buffer, b_read);
+    free(buffer);
+
+    if (b_written < 0)
+        return (0);
+    
+    return (b_written);
+}
