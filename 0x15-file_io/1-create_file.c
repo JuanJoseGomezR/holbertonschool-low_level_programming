9fd923ab4218d4e7e350a000b396131e136148f6
JuanJoseGomezR <juangomez0149@gmail.com>
2020-03-31 22:05:32 +0000

    uploading fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,22 +8,18 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int text_length, fd;
+	int fd, t_len = 0;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-
-	while (*text_content != '\0')
-		text_length++;
-
+	while (*(text_content + t_len))
+		t_len++;
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
 	if (fd == -1)
 		return (-1);
-
-	write(fd, text_content, text_length);
+	write(fd, text_content, t_len);
 	close(fd);
 	return (1);
 }
