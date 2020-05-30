d9348c479f28eec5c76154403d563083b6e32a60
JuanJoseGomezR <juangomez0149@gmail.com>
2020-05-30 04:30:14 +0000

    [Fixed] Code

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,33 +1,38 @@
 #include "hash_tables.h"
 
 /**
-  * hash_table_get - get the value of the idx
-  * @ht: hash table we're gonna use
-  * @key: key we want to get
-  * Return: key value || NULL {if failed
-  */
+ * *hash_table_get - that retrieves a value associated with a key
+ * @ht: the hash table you want to look into
+ * @key: the key you are looking for
+ * Description: return NULL if key couldn't be found
+ * Return: the value associated with the element
+ */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *aux;
 	unsigned long index;
+	hash_node_t *traverse;
 	int i;
 
-	if (ht == NULL || key == NULL)
-		return (NULL);
+	/* return 0 if table, key or value is NULL */
+	if (ht  == NULL || key == NULL)
+		return (NULL); /* return NULL if key can't be found */
 
+	/* use key_index to determine index of the value */
 	index = key_index((const unsigned char *)key, ht->size);
 
-	aux = ht->array[index];
+	traverse = ht->array[index];
 
-	if (aux == NULL)
+	if (traverse == NULL)
 		return (NULL);
 
-	for (i = strcmp(key, aux->key); aux != NULL; )
+	/* handle collision instances */
+	for (i = strcmp(key, traverse->key); traverse != NULL; )
 	{
 		if (i == 0)
-			return (aux->value);
-		aux = aux->next;
+			return (traverse->value);
+		traverse = traverse->next;
 	}
+
 	return (NULL);
 }
