5ca795ccb48867c29df2ded11cab74a35db169b3
JuanJoseGomezR <juangomez0149@gmail.com>
2020-08-25 16:25:57 +0000

    [Fixed] Betty Trailing Whitespaces

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,24 +7,24 @@
  * @array: array given
  * @size: size of array
  * @value: value to find
- * 
+ *
  * Return: value location || -1 if fails
  */
- 
- int linear_search(int *array, size_t size, int value)
- {
-    size_t f;
 
-    if (!array || !*array)
-        return (-1);
+int linear_search(int *array, size_t size, int value)
+{
+	size_t f;
 
-    for (f = 0; f < size; f++)
-    {
-	    printf("Value checked array[%d] = [%d]\n", (int)f, array[f]);
-	    if (array[f] == value)
-	    {
-		    return (f);
-	    }
-    }
-    return (-1);
- }
+	if (!array || !*array)
+		return (-1);
+
+	for (f = 0; f < size; f++)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int)f, array[f]);
+		if (array[f] == value)
+		{
+			return (f);
+		}
+	}
+	return (-1);
+}
